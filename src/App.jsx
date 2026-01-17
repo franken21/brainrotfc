08e7724c72fcb704cc944c9d3530cb07eaed77fa
franken21 <nfranken@gmail.com>
2026-01-17 23:20:45 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MessiVsRonaldo from './pages/MessiVsRonaldo';
 import BrainrotFC from './pages/BrainrotFC';
+import MatchPrototype from './pages/MatchPrototype';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/messi-vs-ronaldo" element={<MessiVsRonaldo />} />
       <Route path="/game" element={<BrainrotFC />} />
+      <Route path="/prototype" element={<MatchPrototype />} />
     </Routes>
   );
 }
